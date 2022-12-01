e02b0ffa8f7052c18799fdecda9ea4e8050935b5
Ola <Ola@DESKTOP-SM5I3NK>
2022-12-01 11:53:23 +0100

    Add project files.

---
+++ EFCoreRelationshipsTutorial/AddWeaponDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCoreRelationshipsTutorial
+{
+    public class AddWeaponDto
+    {
+        public string Name { get; set; } = String.Empty;
+        public int Damage { get; set; } = 10;
+        public int CharacterId { get; set; }
+    }
+}
---
+++ EFCoreRelationshipsTutorial/User.cs
@@ -0,0 +1,10 @@
+﻿namespace EFCoreRelationshipsTutorial
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public List<Character>? Characters { get; set; }
+
+    }
+}
